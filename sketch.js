1c804314a9e3541edec6b8f798ae7de618928614
RishabhKichu <72220922+RishabhKichu@users.noreply.github.com>
2020-10-23 20:59:21 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ var score;
 function preload(){
   
   
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  monkey_running =loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
@@ -78,8 +78,8 @@ function draw() {
   
   if(monkey.isTouching(obstacleGroup)){
     gameState=end;
-    obstacleGroup.setVelocityEachX(0);
-    bananaGroup.setVelocityEachX(0);
+    obstacleGroup.setVelocityXEach(0);
+    bananaGroup.setVelocityXEach(0);
     obstacleGroup.setLifetimeEach(-1);
     bananaGroup.setLifetimeEach(-1);
     
